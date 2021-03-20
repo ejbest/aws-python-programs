c721725a2842eb33531ce47c0fd6f028b830c631
EJ Best <erich.ej.best@gmail.com>
2021-03-20 11:05:50 -0400

    updates on 3/20b

--- key_rotator.py
+++ key_rotator.py
@@ -1,11 +1,10 @@
 import boto3
-<<<<<<< HEAD
 import re
-import time 
 
 file_path = "/Users/ej/.aws/credentials"
 # to test the code change the re_write_path
 re_write_path = file_path
+
 
 def update_secret(secret):
     session = boto3.session.Session()
@@ -15,23 +14,7 @@ def update_secret(secret):
         SecretString=str(secret)
     )
     print("secret updated successfully")
-=======
-import os
-#########################################################################################
-# key_rotator.py                                                                        #
-#                                                                                       #
-#  Function......Will get new keys for all keys in ~/.aws/credentials                   #
-#  Requires......https://aws.amazon.com/sdk-for-python/                                 #
-#  Released......March 11, 2021                                                         #
-#  Scripter......                                                                       #
-#  Invoke........python3 aws-list-services.py                                           #
-#                                                                                       #
-#########################################################################################
-os.system('cp ~/.aws/credentials ~/.aws/credentials.sav')
-file_path = "/Users/ej/.aws/credentials"
-re_write_path = '/Users/ej/.aws/credentials'
 
->>>>>>> 7e77aa7959122eec101550b2cf28cebf0d661439
 
 def rotate_key(old_acc,old_sec):
     iam = boto3.client('iam',aws_access_key_id=old_acc, aws_secret_access_key=old_sec)
@@ -47,15 +30,11 @@ def rotate_key(old_acc,old_sec):
             print("Successfully rotated keys")
             return new_Acc,new_Sec
 
+
 # use this to test
-<<<<<<< HEAD
 def dummy(old_acc, old_Sec):
     return old_acc, old_Sec
 
-=======
-def dummy(acc,sec):
-    return acc,sec
->>>>>>> 7e77aa7959122eec101550b2cf28cebf0d661439
 
 dic = []
 email = []
@@ -63,19 +42,12 @@ index =0
 new_cred = ""
 with open(file_path) as fp:
     data= fp.readlines()
-<<<<<<< HEAD
-=======
-    #print(data)
->>>>>>> 7e77aa7959122eec101550b2cf28cebf0d661439
     for each in data:
         if '#aws_access_key_id' in each:
             acc = each.split("=")[1].strip()
             dic.append({"acc":acc,"sec":""})
             new_cred+="#aws_access_key_id = {}\n"
-<<<<<<< HEAD
 
-=======
->>>>>>> 7e77aa7959122eec101550b2cf28cebf0d661439
         elif '#aws_secret_access_key' in each:
             sec = each.split('=')[1].strip()
             dic[index]["sec"]=sec
@@ -95,7 +67,6 @@ with open(file_path) as fp:
             access_flag=0
             index += 1
         else:
-<<<<<<< HEAD
             match = re.findall(r'[\w\.-]+@[\w\.-]+', each)
             email.extend(match)
             new_cred+=each
@@ -117,19 +88,6 @@ new_cred = new_cred.format(*format_list)
 with open(re_write_path,"w") as fp:
     fp.write(new_cred)
 
-time.sleep(10)
 # write to secrets manager
 update_secret(secret_dic)
 
-=======
-            new_cred+=each
-
-format_list = []
-for each in dic:
-    format_list.extend(rotate_key(each['acc'],each['sec']))
-
-new_cred = new_cred.format(*format_list)
-#print(new_cred)
-with open(re_write_path,"w") as fp:
-    fp.write(new_cred)
->>>>>>> 7e77aa7959122eec101550b2cf28cebf0d661439

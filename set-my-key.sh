554e56fad28148a75ca4785d01b5b842783c7d28
EJ Best <erich.ej.best@gmail.com>
2021-03-20 08:13:44 -0400

    updates and fixed March 18a

--- set-my-key.sh
+++ set-my-key.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -x
 git config --global user.name "EJ Best"
-git config --global user.email "advocatediablo@mail.com"
-eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_san5
+git config --global user.email "erich.ej.best@gmail.com"
+eval "$(ssh-agent -s)" && ssh-add ~/.ssh/id_san6
 

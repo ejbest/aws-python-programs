e09e907d4f3c71ed8f4c7ff84192b53cab8067c1
EJ Best <erich.ej.best@gmail.com>
2021-10-18 09:57:40 -0400

    cleaned up and formated better

--- billing-aws-credentials.py
+++ billing-aws-credentials.py
@@ -1,9 +1,10 @@
 import boto3
 import datetime
 import dateutil.relativedelta
-import pandas  as pd
+import pandas as pd
 import json
 import ast
+import os
 #########################################################################################
 # billing.py                                                                            #
 #                                                                                       #
@@ -12,7 +13,7 @@ import ast
 #  Requires......https://aws.amazon.com/sdk-for-python/                                 #
 #  Released......March 11, 2021                                                         #
 #  Scripter......                                                                       #
-#  Invoke........python3 billing-aws-credentials.py                                           #
+#  Invoke........python3 billing-aws-credentials.py                                     #
 #                                                                                       #
 #########################################################################################
 
@@ -20,24 +21,12 @@ def last_day_of_month(any_day):
     next_month = any_day.replace(day=28) + datetime.timedelta(days=4)
     return next_month - datetime.timedelta(days=next_month.day)
 
-def get_secret(sec_name,region):
-    secret_name = sec_name
-    region_name = region
-    session = boto3.session.Session()
-    client = session.client(service_name='secretsmanager', region_name=region_name)
-    try:
-        get_secret_value_response = client.get_secret_value(SecretId=secret_name)
-        return get_secret_value_response['SecretString']
-    except Exception as e:
-        raise e
-
-
 # get today's date
 today = datetime.datetime.today()
 current_year = today.year
 current_month = '{:02}'.format(today.month)
 current_day ='{:02}'.format(today.day)
-start_day= f"{current_year}-{current_month}-01"
+start_day = f"{current_year}-{current_month}-01"
 current_day = f"{current_year}-{current_month}-{current_day}"
 
 # get last month's first date
@@ -57,8 +46,8 @@ next2_month_year = next2_month_.year
 next2_month_month = '{:02}'.format(next2_month_.month)
 next_month_day_ed = f"{next2_month_year}-{next2_month_month}-01"
 
-myacc_map= get_secret(sec_name='mybilling',region='us-east-1')
-myacc_map = ast.literal_eval(myacc_map)['myacc_map']
+with open(os.path.expanduser("~/.aws/credentials.json")) as f:
+    data = json.load(f)
 
 account = []
 email = []
@@ -66,31 +55,38 @@ previous = []
 current = []
 forecast = []
 rec_lis = []
-# fetch cost
-for each in myacc_map.keys():
-    client = boto3.client('ce', aws_access_key_id=myacc_map[each]['access'], aws_secret_access_key=myacc_map[each]['secret'])
-    # current month
+acc_lis = []
+
+acc_lis = [ ["account_id", "email", "previous", "current", "forecast"] ]
+
+for profile in data["accounts"]:
+    print("reading profile:",profile['profile'])
+    client = boto3.client('ce',aws_access_key_id=profile['access_key'],aws_secret_access_key=profile['secret'])
+
     response_c = client.get_cost_and_usage(TimePeriod={ 'Start': start_day,'End': current_day }, Granularity='MONTHLY', Metrics=["UNBLENDED_COST"], )
     this_month_cost=sum([float(each['Total']['UnblendedCost']['Amount']) for each in response_c['ResultsByTime'] ])
-    # last month
     try:
         response_l = client.get_cost_and_usage(TimePeriod={'Start': last_month_st, 'End': start_day }, Granularity='MONTHLY', Metrics=["UNBLENDED_COST"]    )
         last_month_cost = sum([float(each['Total']['UnblendedCost']['Amount']) for each in response_l['ResultsByTime'] ])
     except Exception as e:
         last_month_cost=0
-    # forecast
     try:
         response_f = client.get_cost_forecast(TimePeriod={'Start': next_month_day_st, 'End': next_month_day_ed}, Granularity='MONTHLY',Metric='NET_UNBLENDED_COST')
         next_month_cost = sum([float(each['MeanValue']) for each in response_f['ForecastResultsByTime'] ])
     except Exception as e:
         next_month_cost=0
-#     # fetch account
-    client = boto3.client("sts", aws_access_key_id=myacc_map[each]['access'], aws_secret_access_key=myacc_map[each]['secret'])
+    #client = boto3.client("sts")
+    client = boto3.client('sts',aws_access_key_id=profile['access_key'],aws_secret_access_key=profile['secret'])
     account_id = client.get_caller_identity()["Account"]
-    rec = {"account_id":account_id,"email":each,"previous":last_month_cost,"current":this_month_cost,"forecast":next_month_cost}
-    print(rec)
-    rec_lis.append(rec)
-df = pd.DataFrame(rec_lis)
-df.to_csv("cost.csv", index=False)
-
+    acc = [account_id, profile['email'], str(last_month_cost), str(this_month_cost), str(next_month_cost)]
+    acc_lis.append(acc)
+    #rec = {"account_id":account_id,"email":profile['email'],"previous":last_month_cost,"current":this_month_cost,"forecast":next_month_cost}
+    #rec_lis.append(rec)
+    
+print("")
+with open("cost.txt", "w") as f:
+    widths = [max(map(len, col)) for col in zip(*acc_lis)]
+    for row in acc_lis:
+        print ("  ".join((val.ljust(width) for val, width in zip(row, widths))))
+        f.write ("  ".join((val.ljust(width) for val, width in zip(row, widths)))+"\n")
 
